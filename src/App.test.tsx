07b50d31045ba39007aeaf4629128251ebc34fa4
Belia Anton <belja.anton@gmail.com>
2024-04-12 17:37:20 +0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Started working with GCP', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const item = screen.getByText(/Started working with GCP/i);
+
+  expect(item).toBeInTheDocument();
 });
